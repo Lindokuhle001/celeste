24f35b5d50de075388f298d645921ac2ed24fb97
lindokuhle shabalala <mlixshaba@gmail.com>
2022-05-20 10:18:09 +0200

    restyled thankYou page

--- pages/thankYou/thankYou.js
+++ pages/thankYou/thankYou.js
@@ -6,23 +6,15 @@ Page({
     email: "",
     table: ""
   },
+
   onLoad() {
-    const app = getApp();
-
-    console.log(app);
-
     this.setData({
       name: store.getState().name,
       email: store.getState().email,
       table: store.getState().table
     });
-
-    console.log(
-      store.getState().table,
-      store.getState().email,
-      store.getState().name
-    );
   },
+  
   redirectToHome() {
     my.redirectTo({
       url: "../index/index"
