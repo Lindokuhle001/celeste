e7558870a365b4d9447c13db657ed368bd180be6
lindokuhle shabalala <mlixshaba@gmail.com>
2022-06-01 09:46:28 +0200

    added authentication

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,26 @@
+import { getAuthCode } from "../../services/authentication";
+import { request } from "../../services/request";
 const app = getApp();
 
 Page({
   data: {
     name: ""
+  },
+
+  onLoad() {
+    const authCode = getAuthCode();
+    // const merchantid = 216620000000188034591;
+    const method = "POST";
+    const headers = {
+      "Content-Type": "application/json"
+    };
+    const url = "http://localhost:3000/login";
+    const data = JSON.stringify({
+      authCode,
+      merchantid: "216620000000188034591"
+    });
+    const response = request(url, headers, data, method);
+    console.log(response);
   },
 
   onShow() {
